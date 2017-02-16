3e1e47f5fbaa64c71eda05e066f9c48814ea82c3
Dmitry V. Alexeev <mail@alexeevdv.ru>
2017-02-16 11:44:03 +0300

    update

--- src/Webmoney.php
+++ src/Webmoney.php
@@ -2,6 +2,22 @@
 
 namespace ejen\payment;
 
+use ejen\payment\webmoney\Payment;
+
 class Webmoney extends \yii\base\Component
 {
+    public $baseUrl = 'https://merchant.webmoney.ru/lmi/payment.asp';
+
+    public $purse;
+    public $resultUrl;
+    public $successUrl;
+    public $successMethod;
+    public $failUrl;
+    public $failMethod;
+
+    public function createPayment($params)
+    {
+        $params['component'] = $this;
+        return new Payment($params);
+    }
 }
---
+++ src/webmoney/Payment.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace ejen\payment\webmoney;
+
+use yii\helpers\Html;
+
+class Payment extends \yii\base\Model
+{
+    public $component;
+
+    public $id;
+    public $amount;
+    public $description;
+    public $email;
+
+    public $purse;
+    public $resultUrl;
+    public $successUrl;
+    public $successMethod;
+    public $failUrl;
+    public $failMethod;
+
+    public function rules()
+    {
+        // @todo Check purse
+        return [];
+    }
+
+    public function getHiddenForm()
+    {
+        $html = Html::beginForm($this->component->baseUrl);
+
+        foreach($this->fields as $name => $value)
+        {
+            $html.= Html::hiddenInput($name, $value);
+        }
+
+        $html.= Html::submitButton();
+        $html.= Html::endForm();
+        return $html;
+    }
+
+    public function getFields()
+    {
+        // Attributes for Payment class only
+        $onlyAttributes = [
+            'id' => 'LMI_PAYMENT_NO',
+            'email' => 'LMI_PAYER_EMAL',
+        ];
+
+        // Attributes both for Payment class and Webmoney component
+        $bothAttributes = [
+            'resultUrl' => 'LMI_RESULT_URL',
+            'successUrl' => 'LMI_SUCCESS_URL',
+            'successMethod' => 'LMI_SUCCESS_METHOD',
+            'failUrl' => 'LMI_FAIL_URL',
+            'failMethod' => 'LMI_FAIL_METHOD',
+        ];
+
+        $fields = [
+            'LMI_PAYEE_PURSE' => $this->purse ? $this->purse : $this->component->purse,
+            'LMI_PAYMENT_AMOUNT' => $this->amount,
+            'LMI_PAYMENT_DESC_BASE64' => base64_encode($this->description),
+        ];
+
+        foreach($onlyAttributes as $attribute => $lmi)
+        {
+            if ($this->{$attribute}) $fields[$lmi] = $this->{$attribute};
+        }
+
+        foreach($bothAttributes as $attribute => $lmi)
+        {
+            if ($this->{$attribute})
+            {
+                $fields[$lmi] = $this->{$attribute};
+            }
+            elseif ($this->component->{$attribute})
+            {
+                $fields[$lmi] = $this->component->{$attribute};
+            }
+        }
+
+        return $fields;
+    }
+}
